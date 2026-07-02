8e29bf295817d1e143f07e2d115e68b82d387884
Soham-Gaikwad-GTasterix <sohamgaikwad@gtasterix.com>
2026-07-02 13:21:39 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post(
                 req.body.password,
                 10
             );
-            const hashedPassword = await User.create({
+            const user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
                 password: hashedPassword,
